e36dc807d27d45296a79c589aaedb3c7acffd5f3
Nathan Turkson <nathanturkson@gmail.com>
2023-01-11 23:50:34 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// Starting the Game based on Any key that is pressed.
+$(document).keypress(function ActionKey() {
+    $("h1").text("Level 1");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
